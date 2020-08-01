9ef453b636b4ceb06c8218370bfa78614d722028
NakasaiHayato <nakasai.hayato@gmail.com>
2020-08-02 00:43:09 +0900

    secret_keyの削除,リンク設定

---
+++ myblogapp/local_settings.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '0s%-93(x95+uy3e@-^7ykka(mqn3%_r9qk8!&kr18v@7%&fp!='
--- myblogapp/settings.py
+++ myblogapp/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+mp1ffto3adrp0q(t%p8n=2!tmrbsr3st7_7ojq+pey703bs^='
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
